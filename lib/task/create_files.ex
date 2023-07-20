755795cdc80817a81c029fae3204d6f972bb51e0
Danyil Mashchenko <6421808+DonCheck@users.noreply.github.com>
2023-07-20 16:40:38 +0300

    Revert "Inspect tests"
    This reverts commit a5acc32a2c93b6f3b3538c7c9c994708ac6c7b36.

--- lib/task/create_files.ex
+++ lib/task/create_files.ex
@@ -64,9 +64,7 @@ defmodule Mix.Tasks.CreateFiles do
 
   defp create_file(params) do
     if Map.get(params, "--a") do
-      IO.inspect("we are here 1")
       create_application_service(params)
-      IO.inspect("we are here 2")
       create_application_service_test(params)
     end
 
